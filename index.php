a36f44451c101dfe4fcab9f471738207d6d9c3a1
moises mora <momora@csumb.edu>
2018-09-10 05:38:45 +0000

    hw1 attempt 3

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html><body>
+<html>
+<body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
@@ -7,5 +8,6 @@
 echo 'Hello world from Cloud9!';
 
 ?>
+<a href="hw/hw1/index.html"> The link</a>
 </body>
 </html>
